46f51a4ea1d62a2d6cb59090531272f2f24a7765
seho <seho.zaq@gmail.com>
2026-04-20 21:26:12 +0800

    Add standard macOS edit menu commands
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- OMOSwitch/App/AppDelegate.swift
+++ OMOSwitch/App/AppDelegate.swift
@@ -16,6 +16,29 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
   }
 
   func applicationDidFinishLaunching(_ notification: Notification) {
+    buildMainMenu()
     statusItemController = container.makeStatusItemController()
+  }
+
+  private func buildMainMenu() {
+    let mainMenu = NSMenu()
+    let appItem = NSMenuItem()
+    mainMenu.addItem(appItem)
+    let appMenu = NSMenu()
+    appMenu.addItem(NSMenuItem(title: "Hide OMO Switch", action: #selector(NSApplication.hide(_:)), keyEquivalent: "h"))
+    appMenu.addItem(NSMenuItem.separator())
+    appMenu.addItem(NSMenuItem(title: "Quit OMO Switch", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+    appItem.submenu = appMenu
+
+    let editItem = NSMenuItem()
+    mainMenu.addItem(editItem)
+    let editMenu = NSMenu(title: "Edit")
+    editMenu.addItem(NSMenuItem(title: "Cut", action: #selector(NSText.cut(_:)), keyEquivalent: "x"))
+    editMenu.addItem(NSMenuItem(title: "Copy", action: #selector(NSText.copy(_:)), keyEquivalent: "c"))
+    editMenu.addItem(NSMenuItem(title: "Paste", action: #selector(NSText.paste(_:)), keyEquivalent: "v"))
+    editMenu.addItem(NSMenuItem(title: "Select All", action: #selector(NSText.selectAll(_:)), keyEquivalent: "a"))
+    editItem.submenu = editMenu
+
+    NSApp.mainMenu = mainMenu
   }
 }
