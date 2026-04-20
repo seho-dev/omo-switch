281f114cfd8b0981483f14377a88121d4f272cb0
seho <seho.zaq@gmail.com>
2026-04-20 23:12:25 +0800

    fix: remove retainedDelegate to resolve Swift 6 concurrency error
    The static property was causing a concurrency-safety error in Swift 6
    strict mode. NSApplication.shared.delegate already retains the delegate,
    so an extra reference is unnecessary.

--- OMOSwitch/App/OMOSwitchApp.swift
+++ OMOSwitch/App/OMOSwitchApp.swift
@@ -1,15 +1,10 @@
 import AppKit
 
 enum OMOSwitchApp {
-  private static var retainedDelegate: AppDelegate?
-
   static func bootstrap(application: NSApplication) {
-    MainActor.assumeIsolated {
-      let delegate = AppDelegate()
-      retainedDelegate = delegate
-      application.setActivationPolicy(.accessory)
-      application.delegate = delegate
-    }
+    let delegate = MainActor.assumeIsolated { AppDelegate() }
+    application.setActivationPolicy(.accessory)
+    application.delegate = delegate
   }
 
 }
